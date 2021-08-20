121f3efe575ac77155d1476a32d56a1070763b2b
Yendric <yendricvanroey@gmail.com>
2021-08-20 16:03:27 +0200

    Verplaatst naar index.js

--- events/ready.js
+++
@@ -1,4 +0,0 @@
-module.exports = (client) => {
-	console.info(`${client.user.username} is online op ${client.guilds.cache.size} server(s)!`);
-	client.user.setActivity('jou', { type: 'WATCHING' });
-};
