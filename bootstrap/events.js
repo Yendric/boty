a554ff11c488d06f6ca71303b28ed972468e35eb
Yendric <yendricvanroey@gmail.com>
2021-08-20 14:31:01 +0200

    Nieuwe bootstrap files

--- bootstrap/events.js
+++ bootstrap/events.js
@@ -1,13 +1,13 @@
 const fs = require('fs');
-const client = require('../index');
+const { client } = require('..');
 
 
 fs.readdir('events/', (err, files) => {
 	if (err) return console.error(err);
 	files.forEach(file => {
-		console.info('Ingeladen event:' + file.split('.')[0]);
 		const event = require(`../events/${file}`);
 		const eventName = file.split('.')[0];
 		client.on(eventName, event.bind(null, client));
+		console.info('Ingeladen event:' + file.split('.')[0]);
 	});
 });
